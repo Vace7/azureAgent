e58220c7a2484d9c8aeb81e4d18675ba9f210fdb
Weng Yi <edwardliew987@gmail.com>
2024-03-08 14:09:46 +0800

    first

---
+++ main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, FileResponse
+from shutil import make_archive
+
+import numpy as np
+import uvicorn
+import os
+
+from DQNAgent import Agent
+
+app = FastAPI()
+agent = Agent()
+
+class Sample(BaseModel):
+    state: list[float]
+    action: int = None
+    reward: int = None 
+    next_state: list[float] = None
+    done: bool = None
+    epsilon: float = None
+
+@app.post("/")
+def home():
+    return "ok"
+
+@app.post("/action")
+def action(sample: Sample):
+    action = agent.get_action(
+        np.array(sample.state), 
+        sample.epsilon
+    )
+    return JSONResponse(content=jsonable_encoder(action))
+
+@app.post("/memorise")
+def memorise(sample: Sample):
+    agent.append_sample(
+        sample.state,
+        sample.action,
+        sample.reward,
+        sample.next_state,
+        sample.done   
+    )
+    return "ok"
+
+@app.post("/update")
+def memorise():
+    agent.update()
+    return "ok"
+
+@app.post("/nextepisode")
+def nextepisode():
+    agent.next_episode()
+    return "ok"
+
+@app.post("/savemodel")
+def savemodel(name : str):
+    agent.save(name)
+    make_archive(name, 'zip', name)
+    return FileResponse(path=os.path.join(os.getcwd(),f"{name}.zip"), filename=f"{name}.zip", media_type="application/zip")
+
+@app.post("/loadmodel")
+def savemodel(name : str):
+    agent.load(name)
+    return "ok"
+
+if __name__ == '__main__':
+    # uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
